398d9d541976a0e2ae58c8ccd84fe60fef4cc4f6
ShadowKyogre <shadowkyogre@aim.com>
2014-11-21 17:23:50 -0800

    Performance enhancement with icon browser

--- new-editor/pyicon_browser.py
+++ new-editor/pyicon_browser.py
@@ -19,9 +19,9 @@ class IcoBrowse(gtk.Dialog):
 		hbox=gtk.HBox(False,2)
 		
 		#format: actual icon, name, context
-		self.model=gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING)
-		self.modelfilter=self.model.filter_new()
-		self.modelfilter.set_visible_func(self.search_icons)
+		#self.model=gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING)
+		#self.modelfilter=self.model.filter_new()
+		self.modelfilter=ICON_STORE.filter_new()
 		self.iconview=gtk.IconView()
 		self.iconview.set_model(self.modelfilter)
 		self.iconview.set_pixbuf_column(0)
@@ -35,22 +35,23 @@ class IcoBrowse(gtk.Dialog):
 		self.refine.connect('changed', self.category_changed)
 		self.refine.set_icon_from_stock(gtk.ENTRY_ICON_SECONDARY, gtk.STOCK_FIND)
 		self.refine.set_size_request(200, 30)
-		list2=[]
+		self.modelfilter.set_visible_func(self.search_icons)
+		#catted_icons=[]
 		for c in defaulttheme.list_contexts():
-			current=defaulttheme.list_icons(context=c)
-			list2+=set(current)
+			#current=defaulttheme.list_icons(context=c)
+			#catted_icons+=set(current)
 			self.combobox.append_text(c)
-			for i in current:
-				try:
-					self.model.append([defaulttheme.load_icon(i, 32,
-									  gtk.ICON_LOOKUP_USE_BUILTIN),
-									  i,c])
-				except GError as err: stderr.write('Error loading "%s": %s\n' % (i, err.args[0]))
-		other=list(set(defaulttheme.list_icons())-(set(list2)))
-		for i in other:
-			self.model.append([defaulttheme.load_icon(i, 32,
-									  gtk.ICON_LOOKUP_USE_BUILTIN),
-									  i,"Other"])
+			#for i in current:
+			#	try:
+			#		self.model.append([defaulttheme.load_icon(i, 32,
+			#						  gtk.ICON_LOOKUP_USE_BUILTIN),
+			#						  i,c])
+			#	except GError as err: stderr.write('Error loading "%s": %s\n' % (i, err.args[0]))
+		#other=list(set(defaulttheme.list_icons())-(set(catted_icons)))
+		#for i in other:
+		#	self.model.append([defaulttheme.load_icon(i, 32,
+		#							  gtk.ICON_LOOKUP_USE_BUILTIN),
+		#							  i,"Other"])
 		self.combobox.prepend_text("Other")
 		scrolled = gtk.ScrolledWindow()
 		scrolled.add(self.iconview)
@@ -69,11 +70,11 @@ class IcoBrowse(gtk.Dialog):
 	def set_defaults(self, icon_name):
 		if icon_name != "" and icon_name != None:
 			cmodel = self.combobox.get_model()
-			for i in xrange(len(self.model)):
-				if self.model[i][1] == icon_name:
+			for i in xrange(len(ICON_STORE)):
+				if ICON_STORE[i][1] == icon_name:
 					self.refine.props.text=icon_name
 					for j in xrange(len(cmodel)):
-						if self.model[i][2] == cmodel[j][0]:
+						if ICON_STORE[i][2] == cmodel[j][0]:
 							print cmodel[j]
 							self.combobox.set_active(j)
 							break
@@ -90,12 +91,38 @@ class IcoBrowse(gtk.Dialog):
 	def search_icons(self, tree, iter):
 		search_term = self.combobox.get_active_text()
 		search_term2 = self.refine.props.text
-		if self.model.get_value(iter, 2) == search_term:
-			if search_term2 == None or search_term2 == "" or re.search(search_term2, self.model.get_value(iter,1)):
+		if ICON_STORE.get_value(iter, 2) == search_term:
+			if search_term2 == None or search_term2 == "" or re.search(search_term2, ICON_STORE.get_value(iter,1)):
 				return True
 			return False
 		else:
 			return False
+
+def set_up():
+	print "Preloading icons"
+	defaulttheme=gtk.icon_theme_get_default()
+	catted_icons=set()
+	for c in defaulttheme.list_contexts():
+		current=defaulttheme.list_icons(context=c)
+		catted_icons=catted_icons.union(set(current))
+		print "Found {} icons in {}".format(len(current),c)
+		#self.combobox.append_text(c)
+		for i in current:
+			try:
+				ICON_STORE.append([defaulttheme.load_icon(i, 32,
+								  gtk.ICON_LOOKUP_USE_BUILTIN),
+								  i,c])
+			except GError as err: stderr.write('Error loading "%s": %s\n' % (i, err.args[0]))
+	other=list(set(defaulttheme.list_icons())-catted_icons)
+	print "Placing misc. icons in Other"
+	for i in other:
+		ICON_STORE.append([defaulttheme.load_icon(i, 32,
+								  gtk.ICON_LOOKUP_USE_BUILTIN),
+								  i,"Other"])
+
+
+ICON_STORE=gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING)
+set_up()
 
 if __name__ == '__main__':
 	icobrowse = IcoBrowse()
