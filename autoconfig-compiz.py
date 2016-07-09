a4f6ab05de314687fa53d958c21a8e73ae2099bf
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-07-09 13:51:02 -0700

    Update convenience config script

--- autoconfig-compiz.py
+++ autoconfig-compiz.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
+from __future__ import print_function
 import compizconfig
 
 context = compizconfig.Context()
 
-if 'run_command0_key' in context.Plugins['core'].Display:
+if 'run_command0_key' in context.Plugins['commands'].Display:
 
-    print "setting up for compiz 0.7.x+"
+    print("setting up for compiz 0.8.x+")
 
-    context.Plugins['core'].Display['command0'].Value = 'compiz-boxmenu'
-    context.Plugins['core'].Display['run_command0_key'].Value = '<Alt>F1'
+    context.Plugins['commands'].Display['command0'].Value = 'compiz-boxmenu'
+    context.Plugins['commands'].Display['run_command0_key'].Value = '<Alt>F1'
 
     if 'vpswitch' in context.Plugins:
         vpswitch = context.Plugins['vpswitch']
         if not vpswitch.Enabled:
             vpswitch.Enabled = True
-        vpswitch.Display['init_plugin'].Value = 'core'
+        vpswitch.Display['init_plugin'].Value = 'commands'
         vpswitch.Display['init_action'].Value = 'run_command0_key'
 
     context.Write()
 
 else:
     
-    print "compiz-boxmenu requires compiz 0.7.x+ for full functionality"
+    print("autoconfig-compiz.py requires compiz 0.8.x+ for full functionality")
